b116acde97d9353492cf15aafe51d97a62a2c14a
zack242 <zakaria_009@hotmail.fr>
2023-02-23 14:59:18 +0100

    add cyclical and historical feature

--- EnergyDisaggregation/energydisaggregation/feature_engineering/cyclical_transformation.py
+++ EnergyDisaggregation/energydisaggregation/feature_engineering/cyclical_transformation.py
@@ -1,2 +1,10 @@
+def cyclical_encoder(df, col, max_val):
+    df = df.copy()
+    # On encode les paramètres cycliques (heure, mois, etc) à l'aide de deux colonnes sin et cos
+    df[col + "_sin"] = np.sin(2 * np.pi * df[col] / max_val)
+    df[col + "_cos"] = np.cos(2 * np.pi * df[col] / max_val)
+    return df.loc[:, [col + "_sin", col + "_cos"]]
+
+
 def test_cyclical_transformation():
     print("Cyclical transformation module can be accessed")
--- EnergyDisaggregation/energydisaggregation/feature_engineering/historical.py
+++ EnergyDisaggregation/energydisaggregation/feature_engineering/historical.py
@@ -1,2 +1,18 @@
+def timeseries_dataframe(df, timelag, column):
+    """Prend en entrée un dataframe avec un seul index (pour une région en particulier),
+    ajoute autant de colonnes que l'on veut avoir de lag"""
+    n = len(df.index)
+    lag_dict = {}
+    for i in range(timelag):
+        data = [float("nan") for a in range(i + 1)]
+        for j in range(n - (i + 1)):
+            value = df.iloc[j + i + 1].loc[column] - df.iloc[j].loc[column]
+            data.append(value)
+        name = f"{column}_lag_{i + 1}"
+        lag_dict[name] = data
+    lag_df = pd.DataFrame(lag_dict)
+    return lag_df
+
+
 def test_historical():
     print("Historical module can be accessed")
