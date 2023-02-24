3014dba93e8c30e94cbcca4d4c759b6ec1a6ced1
zack242 <zakaria_009@hotmail.fr>
2023-02-24 09:42:51 +0100

    fix index in historical

--- EnergyDisaggregation/energydisaggregation/feature_engineering/historical.py
+++ EnergyDisaggregation/energydisaggregation/feature_engineering/historical.py
@@ -1,16 +1,18 @@
+import pandas as pd
+
 def timeseries_dataframe(df, timelag, column):
-    """Prend en entrée un dataframe avec un seul index (pour une région en particulier),
-    ajoute autant de colonnes que l'on veut avoir de lag"""
-    n = len(df.index)
+    index = df.index
+    n = len(index)
     lag_dict = {}
     for i in range(timelag):
-        data = [float("nan") for a in range(i + 1)]
-        for j in range(n - (i + 1)):
-            value = df.iloc[j + i + 1].loc[column] - df.iloc[j].loc[column]
+        data = [float('nan') for a in range(12 * (i + 1))]
+        for j in range(n - 12 * (i + 1)):
+            value = df.iloc[j + 12 * (i + 1)].loc[column] - df.iloc[j].loc[column]
             data.append(value)
-        name = f"{column}_lag_{i + 1}"
+        name = f'{column}_lag_{i + 1}'
         lag_dict[name] = data
     lag_df = pd.DataFrame(lag_dict)
+    lag_df.set_index(index, inplace=True)
     return lag_df
 
 
